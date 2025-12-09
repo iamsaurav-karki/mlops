52d663ecbfd244aeb8a107b3ea234b9124c6ac86
iamsaurav-karki <sauravkarki102@gmail.com>
2025-12-09 13:32:43 +0545

    Created First MLFlow Experiment Project- Model Registry

---
+++ mlflow-starter/1-mlproject/model-registry.py
@@ -0,0 +1,97 @@
+'''
+The MlFlow model Registry component is a centralized model store, set of APIs and UI, to collaboratively 
+manage the full lifecycle of an MLflow Model. It provides model lineage (which run produced this model), 
+model versioning, stage transitions (for example, from staging to production), and annotations.
+
+'''
+import pandas as pd 
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import mlflow 
+import mlflow.sklearn
+from mlflow.models import infer_signature
+
+
+# Set the tracking URI 
+mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
+
+# Load the Dataset 
+X,y = datasets.load_iris(return_X_y=True)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Define the model hyperparameters
+# params = {"penalty": "l2", "solver": "lbfgs", "max_iter": 1000, "random_state": 8888}
+# New paramater 
+params = {"solver": "newton-cg", "max_iter": 1000, "random_state": 1000}
+
+
+# Train the model 
+lr = LogisticRegression(**params)
+lr.fit(X_train, y_train)
+
+
+# Prediction on the test set 
+y_pred = lr.predict(X_test)
+print(y_pred)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
+
+# MlFlow Tracking 
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+# Create a New MLflow Experiment 
+mlflow.set_experiment("Iris-Logistic-Regression-Experiment")
+
+# Start an MLflow Run
+with mlflow.start_run(run_name="Iris-Logistic-Regression-Run"):
+    ## Log Model Hyperparameters
+    mlflow.log_params(params)
+
+    ## Log the accuracy metrics
+    mlflow.log_metric("accuracy", 1.0)
+
+    ## Set a Tag that we can use to remind ouselves what this run was for 
+    mlflow.set_tag("Training Info2", "Basic LR modle for Iris data ")
+
+    ## Infer the model signature 
+
+    signature = infer_signature(X_train,lr.predict(X_train)) # automatically detects the input and output schema for your model by looking at sample data.
+    '''
+    Instead of you manually writing:
+    column names
+    data types
+    output types
+    …it figures all that out for you.
+    '''
+
+    # Log the trained model
+    model_info = mlflow.sklearn.log_model(
+        sk_model=lr,
+        artifact_path="iris_model",
+        signature=signature,
+        input_example=X_train,
+    )
+
+# Inferencing from model from model registry
+
+model_name = "Iris-Logistic-Regression-Model"
+model_version = "latest"  # You can also specify a particular version number
+
+model_uri=f"models:/{model_name}/{model_version}"
+
+model = mlflow.sklearn.load_model(model_uri)
+print("Model loaded from registry:")
+print(model)
+
+print(model_uri)
+
+# Making predictions using the loaded model
+y_pred_new = model.predict(X_test)
+print("Predictions from the loaded model:")
+print(y_pred_new)
